6dfeb5c694f28de4673bef8d195edde324bef1b6
daflyinbed <1677759063@qq.com>
2023-09-02 20:51:51 +0800

    feat(cli): make prev_client* optional

--- torappu/cli/__main__.py
+++ torappu/cli/__main__.py
@@ -8,12 +8,13 @@ from ..models import Version
 parser = argparse.ArgumentParser(
     prog="torappu",
 )
+
 parser.add_argument("client_version")
 parser.add_argument("res_version")
 parser.add_argument(
-    "prev_client_version",
+    "--prev_client_version",
 )
-parser.add_argument("prev_res_version")
+parser.add_argument("--prev_res_version")
 args = parser.parse_args()
 version = Version(res_version=args.res_version, client_version=args.client_version)
 prev = None
