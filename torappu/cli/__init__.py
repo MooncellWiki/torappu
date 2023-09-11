dfe46d6f2c3ae7c89c7c67aaf41e16df05cd8e22
StarHeartHunt <starheart233@gmail.com>
2023-09-11 10:11:18 +0800

    feat: gracefully shutdown

--- torappu/cli/__init__.py
+++ torappu/cli/__init__.py
@@ -4,6 +4,7 @@ import click
 
 from torappu import __version__
 from torappu.core.main import run
+from torappu.log import logger
 
 from ..models import Version
 
@@ -29,10 +30,17 @@ def cli(
     prev_client_version: str,
     prev_res_version: str,
 ):
+    loop = asyncio.get_event_loop()
     version = Version(res_version=res_version, client_version=client_version)
     prev = (
         Version(res_version=prev_res_version, client_version=prev_client_version)
         if prev_client_version and prev_res_version
         else None
     )
-    asyncio.run(run(version, prev))
+    try:
+        loop.run_until_complete(run(version, prev))
+    except KeyboardInterrupt:
+        logger.info("Waiting for tasks to be cancelled (Press Ctrl+C again to exit).")
+        loop.run_until_complete(asyncio.sleep(1))
+    finally:
+        logger.error("exited")
