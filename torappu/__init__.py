e8b7e6a67c194e2ef58b00b033f3b4e9de98f24a
StarHeartHunt <starheart233@gmail.com>
2023-09-12 16:15:13 +0800

    feat: export global config

--- torappu/__init__.py
+++ torappu/__init__.py
@@ -1,6 +1,14 @@
 from importlib.metadata import version
 
+from .config import Config
+
 try:
     __version__ = version("torappu")
 except Exception:
     __version__ = None
+
+_config = Config()
+
+
+def get_config():
+    return _config
