664d3093d0209962e5c033e9a45ab5642909e260
daflyinbed <1677759063@qq.com>
2024-03-07 23:44:37 +0800

    feat: 生成带镀层的蚀刻章套组

--- torappu/core/task/medal_diy.py
+++ torappu/core/task/medal_diy.py
@@ -15,6 +15,7 @@ from .medal_icon import BASE_DIR as MEDAL_ICON_DIR
 
 BASE_DIR = STORAGE_DIR.joinpath("asset", "raw", "medal_diy")
 BKG_DIR = BASE_DIR / "bkg"
+TRIM_DIR = BASE_DIR / "trim"
 
 
 class MedalPosition2DRect(TypedDict):
@@ -35,6 +36,7 @@ class MedalDIY(Task):
 
         self.ab_list = set()
         self.dict_medal_pos: dict[str, list[MedalPosition]] = {}
+        self.dict_advanced: dict[str, str] = {}
 
     @run_sync
     def unpack_metadata(self, ab_path: str):
@@ -50,6 +52,23 @@ class MedalDIY(Task):
 
             self.dict_medal_pos[medal_group_id] = medal_pos_list
 
+    def build_up(self, pos_list: list[MedalPosition], bg: Image.Image):
+        result = bg.copy()
+        for medal_pos in pos_list:
+            medal_image_path = MEDAL_ICON_DIR / f"{medal_pos['medalId']}.png"
+            medal_image = Image.open(medal_image_path)
+
+            # flip the y axis, pillow uses bottom-right as origin
+            result.paste(
+                medal_image,
+                (
+                    int(medal_pos["pos"]["x"] - medal_image.width / 2),
+                    int(bg.height - medal_pos["pos"]["y"] - medal_image.height / 2),
+                ),
+                medal_image,
+            )
+        return result
+
     @run_sync
     def unpack_ab(self, ab_path: str):
         env = UnityPy.load(ab_path)
@@ -63,24 +82,24 @@ class MedalDIY(Task):
                 continue
 
             resized = background_image.resize((1374, 459))
-            for medal_pos in medal_pos_list:
-                medal_image_path = MEDAL_ICON_DIR / f"{medal_pos['medalId']}.png"
-                medal_image = Image.open(medal_image_path)
-
-                # flip the y axis, pillow uses bottom-right as origin
-                resized.paste(
-                    medal_image,
-                    (
-                        int(medal_pos["pos"]["x"] - medal_image.width / 2),
-                        int(
-                            resized.height
-                            - medal_pos["pos"]["y"]
-                            - medal_image.height / 2
-                        ),
-                    ),
-                    medal_image,
-                )
-            resized.save(BASE_DIR / f"{texture.name}.png")
+            self.build_up(medal_pos_list, resized).save(
+                BASE_DIR / f"{texture.name}.png"
+            )
+            if any(medal["medalId"] in self.dict_advanced for medal in medal_pos_list):
+                self.build_up(
+                    [
+                        {
+                            "medalId": (
+                                self.dict_advanced[medal["medalId"]]
+                                if medal["medalId"] in self.dict_advanced
+                                else medal["medalId"]
+                            ),
+                            "pos": medal["pos"],
+                        }
+                        for medal in medal_pos_list
+                    ],
+                    resized,
+                ).save(TRIM_DIR / f"{texture.name}_advanced.png")
 
     def check(self, diff_list: list[Diff]) -> bool:
         diff_set = {diff.path for diff in diff_list}
@@ -107,7 +126,13 @@ class MedalDIY(Task):
         paths = await self.client.resolve_abs(list(self.ab_list))
         BASE_DIR.mkdir(parents=True, exist_ok=True)
         BKG_DIR.mkdir(exist_ok=True)
-
+        TRIM_DIR.mkdir(exist_ok=True)
+        icon_data = self.get_gamedata("excel/medal_table.json")
+        self.dict_advanced = {
+            medal["medalId"]: medal["advancedMedal"]
+            for medal in icon_data["medalList"]
+            if "advancedMedal" in medal and medal["advancedMedal"]
+        }
         metadata_paths = await self.get_metadata_paths()
         async with anyio.create_task_group() as tg:
             for _, ab_path in metadata_paths:
