57a165eb147f85f5bbb09abdf3f53ed53fe18b7e
StarHeartHunt <starheart233@gmail.com>
2024-05-18 12:40:07 +0800

    fix: name conversion

--- torappu/core/task/uniequip_direction.py
+++ torappu/core/task/uniequip_direction.py
@@ -6,6 +6,7 @@ from UnityPy.classes import Sprite
 
 from torappu.models import Diff
 from torappu.consts import STORAGE_DIR
+from torappu.core.client import Client
 
 from .task import Task
 
@@ -15,11 +16,31 @@ BASE_DIR = STORAGE_DIR.joinpath("asset", "raw", "uniequip_direction")
 class UniEquipDirection(Task):
     priority: ClassVar[int] = 3
 
+    def __init__(self, client: Client) -> None:
+        super().__init__(client)
+
+        self.hub_config: dict[str, str] = {}
+
     async def unpack(self, ab_path: str):
         env = UnityPy.load(ab_path)
         for obj in filter(lambda obj: obj.type.name == "Sprite", env.objects):
             texture: Sprite = obj.read()  # type: ignore
-            texture.image.save(BASE_DIR.joinpath(f"{texture.name}.png"))
+            texture.image.save(
+                BASE_DIR.joinpath(f"{self.hub_config[texture.name]}.png")
+            )
+
+    async def unpack_hub(self, ab_path: str):
+        env = UnityPy.load(ab_path)
+        for obj in filter(lambda obj: obj.type.name == "MonoBehaviour", env.objects):
+            behaviour = obj.read_typetree()  # type: ignore
+            # values: Arts/UI/UniEquipDirection/spc-y
+            # keys: spc-y
+            self.hub_config = dict(
+                zip(
+                    [val.split("/")[-1] for val in behaviour["_values"]],
+                    behaviour["_keys"],
+                )
+            )  # type: ignore
 
     def check(self, diff_list: list[Diff]) -> bool:
         diff_set = {diff.path for diff in diff_list}
@@ -34,6 +55,11 @@ class UniEquipDirection(Task):
     async def start(self):
         paths = await self.client.resolve_abs(list(self.ab_list))
         BASE_DIR.mkdir(parents=True, exist_ok=True)
+
+        hub_ab_path = await self.client.resolve_ab(
+            self.client.asset_to_bundle["arts/ui/uniequipdirection/pic_hub"][:-3]
+        )
+        await self.unpack_hub(hub_ab_path)
 
         async with anyio.create_task_group() as tg:
             for _, ab_path in paths:
