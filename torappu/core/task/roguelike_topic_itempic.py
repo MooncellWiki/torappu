7d07c91cc4652df9c284dd8d455607c709495a8c
StarHeartHunt <starheart233@gmail.com>
2025-01-07 22:56:20 +0800

    feat: roguelike topic item pic

---
+++ torappu/core/task/roguelike_topic_itempic.py
@@ -0,0 +1,42 @@
+from typing import TYPE_CHECKING, ClassVar
+
+import anyio
+import UnityPy
+
+from torappu.consts import STORAGE_DIR
+from torappu.models import Diff
+
+from .task import Task
+
+if TYPE_CHECKING:
+    from UnityPy.classes import Sprite
+
+BASE_DIR = STORAGE_DIR.joinpath("asset", "raw", "roguelike_topic_itempic")
+
+
+class RoguelikeTopicItempic(Task):
+    priority: ClassVar[int] = 2
+
+    async def unpack(self, ab_path: str):
+        env = UnityPy.load(ab_path)
+        for obj in filter(lambda obj: obj.type.name == "Sprite", env.objects):
+            texture: Sprite = obj.read()  # type: ignore
+            texture.image.save(BASE_DIR.joinpath(f"{texture.name}.png"))
+
+    def check(self, diff_list: list[Diff]) -> bool:
+        diff_set = {diff.path for diff in diff_list}
+        self.ab_list = {
+            bundle
+            for asset, bundle in self.client.asset_to_bundle.items()
+            if asset.startswith("arts/ui/rogueliketopic/itempic") and bundle in diff_set
+        }
+
+        return len(self.ab_list) > 0
+
+    async def start(self):
+        paths = await self.client.resolves(list(self.ab_list))
+        BASE_DIR.mkdir(parents=True, exist_ok=True)
+
+        async with anyio.create_task_group() as tg:
+            for _, ab_path in paths:
+                tg.start_soon(self.unpack, ab_path)
